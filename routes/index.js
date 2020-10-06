90733c5169912351ed4bf2ad4f414af3f6e61e69
915dbfl <64644738+915dbfl@users.noreply.github.com>
2020-10-07 03:12:13 +0900

    router 수정(진행)

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('main_screen', { title: '라라라' });
+router.get('/', function(req, res) {
+  res.render('main_screen', { title: 'main' });
+});
+
+router.get('/dignosis', function(req, res) {
+  res.render('dignosis', { title: 'dignosis' });
+});
+
+router.get('/dignosis_headache.html.com', function(req, res) {
+  res.render('dignosis_headache', { title: 'headache' });
+});
+
+
+
+
+router.get('/search', function(req, res) {
+  res.render('search', { title: 'search' });
+});
+
+router.get('/introduction', function(req, res) {
+  res.render('introduction', { title: 'introduction' });
 });
 
 
