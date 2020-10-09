d5bfabd5f4dff7f127212ab755446f3f57f8c6af
915dbfl <64644738+915dbfl@users.noreply.github.com>
2020-10-09 19:53:08 +0900

    MySQL DB 연동 완료!!

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@ const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
+
+// mysql 모듈 불러오기
+// const mysql = require('mysql');
+// const dbconfig = require('./config/database.js');
+// const connection = mysql.createConnection(dbconfig);
+
+var db_config = require('./config/database.js');
+var conn = db_config.init();
+
+db_config.connect(conn);
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('main_screen', { title: 'main' });
@@ -13,10 +25,13 @@ router.get('/dignosis', function(req, res) {
 
 
 router.get('/dignosis/:dignosisId', function(req, res){
-  res.render('./dignosis/' + req.params.dignosisId, {title: 'hihi'})
+  var sql = 'SELECT * from symptom WHERE part="머리"';
+  conn.query(sql, function(err, rows, fields){
+    console.log(rows);
+    if(err) console.log('query is not excuted. insert fail...\n' + err);
+    else res.render('./dignosis/' + req.params.dignosisId, {contents : rows[0]});  
+  });
 });
-
-
 
 router.get('/search', function(req, res) {
   res.render('search', { title: 'search' });
